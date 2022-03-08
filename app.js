f70fdba267cfd2bb71afd773e2f109133571420e
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-03-08 09:52:20 +0430

    Add eventlistener to humburger and navmenu and fixed miss functionality

--- app.js
+++ app.js
@@ -5,8 +5,3 @@ hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
