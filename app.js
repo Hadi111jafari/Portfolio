7a4e9df1f404ced59540c3e31b56fc2d304cc915
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-03-08 22:17:01 +0430

    Add the configurations of the JavaScript linters and fix the issues

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", function() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }));
-
