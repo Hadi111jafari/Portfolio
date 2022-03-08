7a047134664abeb758a767fa58f88e1e41b324f5
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-03-08 20:39:31 +0430

    fix linter errors

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function() {
+    btn.classList.add("active");
+});
+
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+const card = document.querySelector(".card");
+
+card.addEventListener("click", function() {
+    card.classList.add("active");
+});
