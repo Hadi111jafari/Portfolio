b245220cef31cee5997f12ac0ff78e2eb07d1b6f
OMSOO <43151556+omsoo@users.noreply.github.com>
2022-03-08 16:56:46 +0100

    Update the nav link in app.js file and formatting
    Work on updating the app.js file

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
