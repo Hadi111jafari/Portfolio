ec3665bf4f96c91bd5b593050c5825c534227f6f
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-03-09 20:59:59 +0430

    applied required changes

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  document.querySelector('body').classList.toggle('active');
 });
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
+  document.querySelector('body').classList.toggle('active');
 }));
